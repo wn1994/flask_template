f80dccb7b1c839812686ea97db203d7ddf5f3e2c
wangning <1017888485@qq.com>
2018-05-08 12:36:17 +0800

    init

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-user_config = dict(
-    DATABASE=os.path.join(os.path.dirname(__file__), 'flaskr.db'),
-    # DEBUG=True,
-    # SECRET_KEY=b'_5#y2L"F4Q8z\n\xec]/',
-    # USERNAME='admin',
-    # PASSWORD='admin'
-)
